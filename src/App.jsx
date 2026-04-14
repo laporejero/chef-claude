2e4df2eec77697e7a702a2fc888a5d0bed576640
laporejero <laporejero1297@gmail.com>
2026-04-14 14:17:10 +0800

    added MainContent component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Header from './Header'
+import Main from './MainContent'
 import './App.css'
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
