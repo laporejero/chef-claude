c548eb2f8139e89be5fe185029037a25eadd6a5c
laporejero <laporejero1297@gmail.com>
2026-04-17 10:52:00 +0800

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -33,13 +33,17 @@ export default function Main() {
                 <section>
                     <h2>Ingredients on hand:</h2>
                     <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                    <div className="get-recipe-container">
-                        <div>
-                            <h3>Ready for a recipe?</h3>
-                            <p>Generate a recipe from your list of ingredients.</p>
+                    {
+                        // if the ingredients list is more than 3, display the get-recipe-container div
+                        ingredients.length > 3 &&
+                        <div className="get-recipe-container">
+                            <div>
+                                <h3>Ready for a recipe?</h3>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button>Get a recipe</button>
                         </div>
-                        <button>Get a recipe</button>
-                    </div>
+                    }
                 </section>
             }
             
